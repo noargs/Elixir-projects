ae3a3dc5ed18f190c4c191f3e85d12562cb84c76
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-20 20:22:02 +0000

    Coordinator to get results back for further processing

---
+++ weather_httpoison/lib/coordinator.ex
@@ -0,0 +1,22 @@
+defmodule WeatherHttpoison.Coordinator do
+  @moduledoc """
+  Coordinator module to get the results for further
+  processing i.e. sorting, keeping state
+  """
+
+  def loop(results \\ [], results_expected) do
+    receive do
+      {:ok, result} ->
+        new_results = [result|results]
+        if results_expected == Enum.count(new_results) do
+          send self, :exit
+        end
+        loop(new_results, results_expected)
+      :exit ->
+        IO.puts(results |> Enum.sort |> Enum.join(", "))
+      _ ->
+        loop(results, results_expected)
+    end
+  end
+
+end
