edee694c010812a1909bd5a95889f6a360fca5ef
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-20 06:05:12 +0000

    init project

---
+++ weather_httpoison/lib/weather_httpoison.ex
@@ -0,0 +1,18 @@
+defmodule WeatherHttpoison do
+  @moduledoc """
+  Documentation for `WeatherHttpoison`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WeatherHttpoison.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ weather_httpoison/test/weather_httpoison_test.exs
@@ -0,0 +1,8 @@
+defmodule WeatherHttpoisonTest do
+  use ExUnit.Case
+  doctest WeatherHttpoison
+
+  test "greets the world" do
+    assert WeatherHttpoison.hello() == :world
+  end
+end
