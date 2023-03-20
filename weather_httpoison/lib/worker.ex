e213a29b0d39d09a8de78f9558eebc1063d8dc4f
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-20 06:39:14 +0000

    encode url

--- weather_httpoison/lib/worker.ex
+++ weather_httpoison/lib/worker.ex
@@ -6,7 +6,12 @@ defmodule WeatherHttpoison.Worker do
 
 
   def temperature_of(location) do
+    location
+    |> url_for(location)
+  end
 
+  defp url_for(location) do
+    URI.encode()
   end
 
 end
