c0cc1aab188eea73396117e56a679d277e22dc7a
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-21 23:54:28 +0000

    ring of linked processes to propogate error down the chain

---
+++ linked_processes_ring/lib/linked_processes_ring.ex
@@ -0,0 +1,18 @@
+defmodule LinkedProcessesRing do
+  @moduledoc """
+  Documentation for `LinkedProcessesRing`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LinkedProcessesRing.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
